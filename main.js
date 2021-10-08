1b56604104317a35ddf0d8d52e74a51600258d91
Cool-Programmer <margaryan.mher.28@gmail.com>
2021-10-08 21:10:03 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,111 @@
+import './style.css'
+
+import * as THREE from "three";
+
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGL1Renderer({
+  canvas: document.querySelector("#bg")
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 2, 16, 80);
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(20, 20, 20);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+// Watch mouse movement
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Randomly add stars
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+// Populate stars
+Array(200).fill().forEach(addStar);
+
+// Add space texture
+const spaceTexture = new THREE.TextureLoader().load("images/space.jpg");
+scene.background = spaceTexture;
+
+// Add my picture as cube
+const myPic = new THREE.TextureLoader().load("images/mher.jpg");
+const myCube = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial({map: myPic})
+);
+
+scene.add(myCube);
+
+// Add moon to space
+const moonTexture = new THREE.TextureLoader().load("images/moon.jpg");
+const normalTexture = new THREE.TextureLoader().load("images/normal.jpg");
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture
+  })
+);
+scene.add(moon);
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+myCube.position.z = -5;
+myCube.position.x = 2;
+
+
+// Move camera
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  myCube.rotation.y += 0.01;
+  myCube.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+document.body.onscroll = moveCamera;
+moveCamera();
+
+// Main loop
+function animate() {
+  requestAnimationFrame(animate);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+  controls.update();
+  renderer.render(scene, camera);
+}
+animate();
